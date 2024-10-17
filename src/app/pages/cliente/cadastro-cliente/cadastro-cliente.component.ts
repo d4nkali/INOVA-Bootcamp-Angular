f61fdcfe2b16da68426a5be53b97c350be5df93f
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-10-17 16:29:05 -0300

    Codando ate topico 23

--- src/app/pages/cliente/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/pages/cliente/cadastro-cliente/cadastro-cliente.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Cliente } from 'src/app/shared/models/Cliente';
 import { ClienteService } from 'src/app/shared/services/cliente.service';
 import Swal from 'sweetalert2';
@@ -12,10 +12,11 @@ import Swal from 'sweetalert2';
   styleUrls: ['./cadastro-cliente.component.scss']
 })
 export class CadastroClienteComponent implements OnInit{
+  editar;
   formGroup: FormGroup;
 
 
-  constructor(private clienteService: ClienteService, private router: Router){
+  constructor(private clienteService: ClienteService, private router: Router, private route: ActivatedRoute){
     this.formGroup = new FormGroup({
       id: new FormControl(null),
       nome: new FormControl('', Validators.required),
@@ -24,34 +25,67 @@ export class CadastroClienteComponent implements OnInit{
       observacoes: new FormControl('', Validators.required),
       ativo: new FormControl(true)
     });
+    this.editar = false
   }
 
 
   ngOnInit(): void {
-
+    if (this.route.snapshot.params["id"]){
+      this.editar = true
+      this.clienteService.pesquisarPorId(this.route.snapshot.params["id"]).subscribe(
+        cliente => {
+          this.formGroup.patchValue(cliente)
+        }
+      )
+    }
   }
 
 
   cadastrar() {
     const cliente: Cliente = this.formGroup.value;
-    this.clienteService.inserir(cliente).subscribe({
-      next: () => {
-        Swal.fire({
-          icon: 'success',
-          title: 'Sucesso',
-          text: 'Cliente cadastrado com sucesso!',
-          showConfirmButton: false,
-          timer: 3000
-        })
-        this.router.navigate(['/cliente'])
-      },
-      error: (error) => {
-        console.error(error)
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Erro ao cadastrar cliente!',
-        })
-      }})
+    if (this.editar) {
+      this.clienteService.atualizar(cliente).subscribe({
+        next: () => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Sucesso',
+            text: 'Cliente atualizado com sucesso!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          this.router.navigate(['/cliente']);
+        },
+        error: (error) => {
+          console.error(error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Erro ao atualizar cliente!',
+          });
+        }
+      });
+    } else {
+      // Modo de criação
+      this.clienteService.inserir(cliente).subscribe({
+        next: () => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Sucesso',
+            text: 'Cliente cadastrado com sucesso!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          this.router.navigate(['/cliente']);
+        },
+        error: (error) => {
+          console.error(error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Erro ao cadastrar cliente!',
+          });
+        }
+      });
     }
   }
+}
